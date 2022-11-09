6a068b1d7491b02d27d835b50ae9ffcd73a1032e
badkapav <avigyandas123@gmail.com>
2022-11-09 23:09:05 +0530

    initial commit

--- app/src/main/java/com/example/vigor/pageFragments/HelpFragment.kt
+++ app/src/main/java/com/example/vigor/pageFragments/HelpFragment.kt
@@ -1,10 +1,11 @@
-package com.example.vigor
+package com.example.vigor.pageFragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.vigor.R
 
 class HelpFragment : Fragment() {
 
